0f3e236a904ba71573e0916da5a2b3dd280d2929
Mert Çetin <mrtctn506@gmail.com>
2023-08-01 17:23:44 +0300

    List.jsx finished

--- src/components/Liste.jsx
+++ src/components/Liste.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useState } from 'react'
 
 function Liste() {
+
+    const options = ['Film izle', 'Dışarı çık', 'Yemek ye', 'Kitap Oku', 'Oyun izle'];
+    const [option, setOption] = useState('');
+
+    const optionsRandom = () => {
+        const randomIndex = Math.floor(Math.random() * options.length);
+        const randomOption = options[randomIndex];
+        setOption(randomOption);
+    }
+
+    const onReset = () => {
+        setOption('');
+    }
+
+
     return (
-        <div>Liste</div>
+        <div>
+            <button onClick={optionsRandom}>Random</button>
+            <button onClick={onReset}>Reset</button>
+            <div>
+                {options.map((item, id) => (
+                    <h2 key={id}>{item}</h2>
+                ))}
+            </div>
+            <br />
+            {option && (
+                <div>
+                    <h1>Random Seçenek:</h1>
+                    <h1>{option}</h1>
+                </div>
+            )}
+        </div>
     )
 }
 
-export default Liste
+export default Liste;
